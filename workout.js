5bbafbf0bee7ca65b034a6c24f34f65127d3d392
gowthamC8754 <20ubc648@mail.sjctni.edu>
2023-10-11 21:45:19 -0700

    todolist

---
+++ workout.js
@@ -0,0 +1,16 @@
+let arr=[10,20,30,40,50];
+
+let result = arr.map(divide)
+
+function divide(num){
+    return num * 5
+}
+
+
+console.log(result);
+
+let flitter=[10,20,30 ,40,50] 
+flitter.forEach(val=>console.log(val))
+ function conditoin(val){
+    console.log(val)
+ }
